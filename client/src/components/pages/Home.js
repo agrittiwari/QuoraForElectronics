6f1b8290e2d976471818c37b1578fe7aebc029be
Akshays133 <akshays1333@gmail.com>
2021-12-14 11:45:16 +0530

    questions update

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -6,13 +6,13 @@ export class Home extends Component {
             <div>
                 <h1 className='text-yellow'>Recent Question</h1>
                 <div>
-                    <ol>
+                <ol>
                         <li><h2>How to identify Base, Emitter, and collector of a Transistor?</h2></li>
-                        <li><h2>How to identify input, output, and ground of 7805 / 7812?</h2></li>
-                        <li><h2>What are the types of Resistors?</h2></li>
-                        <li><h2>What is the difference between 89c51 and 89s51?</h2></li>
+                        <li><h2>How to integrate LCD display module with ESP8266?</h2></li>
+                        <li><h2>What are the resources to learn about robotics and embedded systems?</h2></li>
+                        <li><h2>How to implement certain RISC- CISC architecture?</h2></li>
                         <li><h2>What is ISP connector? what are the pin connections of ISP connector?</h2></li>
-                        <li><h2>Which software have you used for PCB layout? What were the other options?</h2></li>
+                        <li><h2>Which software have you used for PCB layout? What were the other options? Please verify my PCB design</h2></li>
                     </ol>
                 </div>
                 <div className='comment'>
--- client/src/components/pages/dashBoard.js
+++ client/src/components/pages/dashBoard.js
@@ -5,14 +5,20 @@ const dashBoard = () => {
         <div>
             <h1 className='text-yellow'>Recent Question</h1>
                 <div>
-                    <ol>
+                    {/* <ol>
                         <li><h2>How to identify Base, Emitter, and collector of a Transistor?</h2></li>
                         <li><h2>How to integrate LCD display module with ESP8266?</h2></li>
                         <li><h2>What are the resources to learn about robotics and embedded systems?</h2></li>
                         <li><h2>How to implement certain RISC- CISC architecture?</h2></li>
                         <li><h2>What is ISP connector? what are the pin connections of ISP connector?</h2></li>
                         <li><h2>Which software have you used for PCB layout? What were the other options? Please verify my PCB design</h2></li>
-                    </ol>
+                    </ol> */}
+ 
+ 
+ <h1>Dashboard </h1>
+ <h2>Work in progress</h2>
+ 
+ 
                 </div>
                 <div className='comment'>
                     <label htmlFor='comment'>Comment</label>
